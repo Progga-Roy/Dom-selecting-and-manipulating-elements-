096d40ff8cec430fc4c22412b410ed9dc3f8c759
Progga Roy~ <proggaroy2000@gmail.com>
2023-09-17 01:22:11 +0600

    explore style,get n setAttibutes, innerHtml

--- dom.js
+++ dom.js
@@ -34,3 +34,31 @@ for (const sLi of someLi) {
 // querySelector
 const someLis = document.querySelector('#fruit-title')
 console.log(someLis)
+
+
+//  change property using Style
+const styleDress = document.getElementById('dress')
+styleDress.style.backgroundColor = 'black'
+styleDress.style.textAlign = 'center'
+styleDress.textContent = 'I have huge dress collection'
+styleDress.style.color ='white'
+styleDress.style.padding ='10px 10px'
+styleDress.style.boxShadow ='10px 10px 5px grey'
+
+
+// console.log(styleDress)
+
+// get and setAttribute
+const findDress = document.getElementById('dress')
+// findDress.getAttribute('id')
+findDress.setAttribute('title',' tooltip My favorite dresses which I usually ware.')
+
+// console.log(findDress)
+
+
+// user innerHTML for change title
+const fruit = document.getElementsByClassName('fruits-container')[0].innerHTML ='<h2>Fruit title become flower title, what a change!!</h2>'
+
+console.log(fruit)
+
+
